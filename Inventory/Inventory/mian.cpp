66de6b7dd4625249a8b22541aaf662a3e28688e6
NMEii <82252932+NMEii@users.noreply.github.com>
2025-12-18 16:18:12 +0900

    인벤토리 타입 생성 / 기타 장비 포션

--- Inventory/Inventory/mian.cpp
+++ Inventory/Inventory/mian.cpp
@@ -6,14 +6,22 @@ using namespace std;
 class item
 {
 private:
+	string type;
 	string itemName;
 	int price;
 
 public:
 	item(string name = "", int _price = 0) :
+		type("기타"),
 		itemName(std::move(name)),
 		price(_price)
 	{
+
+	}
+
+	void PrintType()
+	{
+		cout << "[  " << type << "  ]" << endl;
 
 	}
 
@@ -23,16 +31,75 @@ public:
 	}
 };
 
+class Weapon 
+{
+private:
+	string type;
+	string weaponName;
+	int price;
+
+public:
+	Weapon(string name = "", int _price = 0) :
+		type("장비"),
+		weaponName(name),
+		price(_price)
+	{
+
+	}
+	
+	void PrintType()
+	{
+		cout << "[  " << type << "  ]" << endl;
+
+	}
+
+	void Print() const
+	{
+		cout << "[ 이름 : " << weaponName << ", 가격 : " << price << "G ]" << endl;
+	}
+};
+
+class Potion 
+{
+private:
+	string type;
+	string potionName;
+	int price;
+
+public:
+	Potion(string name = "", int _price = 0) :
+		type("포션"),
+		potionName(name),
+		price(_price)
+	{
+
+	}
+
+	void PrintType()
+	{
+		cout << "[  " << type << "  ]" << endl;
+
+	}
+
+	void Print() const
+	{
+		cout << "[ 이름 : " << potionName << ", 가격 : " << price << "G ]" << endl;
+	}
+};
+
+
 template<typename T>
 class Inventory
 {
 private:
+	string invenType;
 	T* pItem;
 	int capacity;
 	int size;
 
 public:
 	Inventory(int _capacity = 10) :
+		invenType(""),
 		pItem(new T[_capacity]),
 		capacity(_capacity),
 		size(0)
@@ -62,6 +129,11 @@ public:
 
 	int getCapacity() const { return capacity; }
 
+	void PrintInvenType() const
+	{
+		pItem->PrintType();
+	}
+
 	void PrintItem() const
 	{
 		for (int i = 0; i < size; ++i)
@@ -75,18 +147,31 @@ public:
 int main()
 {
 	Inventory<item>* itemInventory = new Inventory<item>();
+	Inventory<Weapon>* weaponInventory = new Inventory<Weapon>();
+	Inventory<Potion>* potionInventory = new Inventory<Potion>();
 
-	for (int i = 0; i < 10; i++)
-	{
-		item _item= item("item", i * 100);
-		itemInventory->AddItem(_item);
-	}
-	itemInventory->removeLastItem();
+	item _item = item("나무", 30);
+	itemInventory->AddItem(_item);
+	
+	Weapon _weapon = Weapon("롱소드", 350);
+	weaponInventory->AddItem(_weapon);
+	
+	Potion _potion = Potion("회복포션", 50);
+	potionInventory->AddItem(_potion);
 
+	itemInventory->PrintInvenType();	// 인벤토리 타입 출력
 	itemInventory->PrintItem();
 	
+	weaponInventory->PrintInvenType();
+	weaponInventory->PrintItem();
 	
+	potionInventory->PrintInvenType();
+	potionInventory->PrintItem();
+
 	delete itemInventory;
+	delete weaponInventory;
+	delete potionInventory;
+
 	itemInventory = nullptr;
 
 	return 0;
